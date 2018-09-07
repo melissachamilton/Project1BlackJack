71e79d09c996223fb81f8ca4fe0e01f3651bab2b
melissachamilton <melissac.hamilton1@gmail.com>
2018-09-07 12:46:36 -0400

    workingalerts

--- main.js
+++ main.js
@@ -1,76 +1,76 @@
 var cards = [{
-        Name: "King",
-        Value: 10
-    },
-    {
-        Name: "Queen",
-        Value: 10
-    },
-    {
-        Name: "Jack",
-        Value: 10
-    },
-    {
-        Name: "Card10",
-        Value: 10
-    },
-    {
-        Name: "Card9",
-        Value: 9
-    },
-    {
-        Name: "Card8",
-        Value: 8
-    },
-    {
-        Name: "Card7",
-        Value: 7
-    },
-    {
-        Name: "Card6",
-        Value: 6
-    },
-    {
-        Name: "Card5",
-        Value: 5
-    },
-    {
-        Name: "Card4",
-        Value: 4
-    },
-    {
-        Name: "Card3",
-        Value: 3
-    },
-    {
-        Name: "Card2",
-        Value: 2
-    },
-    {
-        Name: "Card1",
-        Value: 1
-    },
+    Name: "King",
+    Value: 10
+},
+{
+    Name: "Queen",
+    Value: 10
+},
+{
+    Name: "Jack",
+    Value: 10
+},
+{
+    Name: "Card10",
+    Value: 10
+},
+{
+    Name: "Card9",
+    Value: 9
+},
+{
+    Name: "Card8",
+    Value: 8
+},
+{
+    Name: "Card7",
+    Value: 7
+},
+{
+    Name: "Card6",
+    Value: 6
+},
+{
+    Name: "Card5",
+    Value: 5
+},
+{
+    Name: "Card4",
+    Value: 4
+},
+{
+    Name: "Card3",
+    Value: 3
+},
+{
+    Name: "Card2",
+    Value: 2
+},
+{
+    Name: "Card1",
+    Value: 1
+},
 ]
 
 var playerHand = []
 
 var playerOne = function () {
-    playerHand.push(cards[Math.floor(Math.random() * cards.length)]);
-    playerHand.push(cards[Math.floor(Math.random() * cards.length)]);
-    console.log("PLAYER HAND", playerHand)
-    
+playerHand.push(cards[Math.floor(Math.random() * cards.length)]);
+playerHand.push(cards[Math.floor(Math.random() * cards.length)]);
+console.log("PLAYER HAND", playerHand)
+
 }
 
 playerHandSum = () => {
-    sum = 0
-    for (var i = 0; i < playerHand.length; i++) {
-        var aCard = playerHand[i]
-        console.log(aCard.Value)
-        var cardValue = aCard.Value
-        sum += cardValue
-    }
+sum = 0
+for (var i = 0; i < playerHand.length; i++) {
+    var aCard = playerHand[i]
+    console.log(aCard.Value)
+    var cardValue = aCard.Value
+    sum += cardValue
+}
 
-    return sum;
+return sum;
 }
 
 playerHandSum()
@@ -78,67 +78,66 @@ playerHandSum()
 var dealerHand = [];
 var dealer = function () {
 
-    dealerHand.push(cards[Math.floor(Math.random() * cards.length)]);
-    dealerHand.push(cards[Math.floor(Math.random() * cards.length)]);
-    console.log("DEALER HAND", dealerHand)
+dealerHand.push(cards[Math.floor(Math.random() * cards.length)]);
+dealerHand.push(cards[Math.floor(Math.random() * cards.length)]);
+console.log("DEALER HAND", dealerHand)
 }
 
 window.onload = function () {
-    $('#start').click(dealer)
-    $('#start').click(playerOne)
-    $('#total').on('click', function () {
-        playerSum = 0
-        for (var i = 0; i < playerHand.length; i++) {
-            var aCard = playerHand[i]
-            var cardValue = aCard.Value
-            playerSum += cardValue
-            // console.log("PLAYER SUM", playerSum)
+$('#start').click(dealer)
+$('#start').click(playerOne)
+$('#total').on('click', function () {
+    playerSum = 0
+    for (var i = 0; i < playerHand.length; i++) {
+        var aCard = playerHand[i]
+        var cardValue = aCard.Value
+        playerSum += cardValue
+        // console.log("PLAYER SUM", playerSum)
 
-        }
-        return playerSum
-    })
+    }
+    return playerSum
+})
 
-    $('#stay').on('click', function () {
-        dealerSum = 0
-        Message = "Player Wins!"
-        Message = "House Wins!"
+$('#stay').on('click', function () {
+    dealerSum = 0
+    Message = "Player Wins!"
+    Message = "House Wins!"
 
-        if (playerSum >= dealerSum) {
-            dealerHand.push(cards[Math.floor(Math.random() * cards.length)]);
-        }
-        for (var i = 0; i < dealerHand.length; i++) {
-            var aCard = dealerHand[i]
-            var cardValue = aCard.Value
-            dealerSum += cardValue
-            // console.log("DEALER SUM", dealerSum)
+    if (playerSum >= dealerSum) {
+        dealerHand.push(cards[Math.floor(Math.random() * cards.length)]);
+    }
+    for (var i = 0; i < dealerHand.length; i++) {
+        var aCard = dealerHand[i]
+        var cardValue = aCard.Value
+        dealerSum += cardValue
+        // console.log("DEALER SUM", dealerSum)
 
-        }
-        if (dealerSum > 21) {
-            Message = "Player Wins!";
-            // console.log('Player Wins!')
-        }
-    
-        if (dealerSum > playerSum) {
-            Message = "House Wins!";
-        } else {
-            Message = "Player Wins!";
-        }
-        if (dealerSum > 21) {
-            Message = "Player Wins!";
-        }
-        return dealerSum
-    });
+    }
+    if (dealerSum > 21) {swal(
+        "Player Wins!");
+    }
 
-    $('#hit').on('click', function () {
-        playerHand.push(cards[Math.floor(Math.random() * cards.length)]);
-        console.log("PLAYERHAND", playerHand)
-    });
+    if (dealerSum > playerSum) {swal(
+        "House Wins!");
+    } else {swal(
+        "Player Wins!");
+    }
+    if (dealerSum > 21) {swal(
+        "Player Wins!");
+    }
+    return dealerSum
+});
+
+$('#hit').on('click', function () {
+    playerHand.push(cards[Math.floor(Math.random() * cards.length)]);
+    console.log("PLAYERHAND", playerHand)
+});
 }
 
 // $('button').on('click', () => {
-    // $('.message').html('Player Wins!!!')
+// $('.message').html('Player Wins!!!')
 // })
 
-$('button').on('click', () => {
-    $('.message').html('Player W!!!')
-});
+// $('button').on('click', () => {
+// $('.message').html('Player W!!!')
+// });
